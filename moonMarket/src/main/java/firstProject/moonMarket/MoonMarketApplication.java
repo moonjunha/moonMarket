28994ccae7ca0b9655a195fcbb9e149857f933e0
moonjunha <75133229+moonjunha@users.noreply.github.com>
2022-10-02 18:11:27 +0900

    first

---
+++ moonMarket/src/main/java/firstProject/moonMarket/MoonMarketApplication.java
@@ -0,0 +1,13 @@
+package firstProject.moonMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonMarketApplication.class, args);
+	}
+
+}
---
+++ moonMarket/src/test/java/firstProject/moonMarket/MoonMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package firstProject.moonMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
